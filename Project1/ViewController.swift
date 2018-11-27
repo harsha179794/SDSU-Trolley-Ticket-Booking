e475c14e57211e450cd320cad5eb9bc653c745a0
Venkata Satya Harsha Vardhan Lolla <harshavardhan@venkatas-macbook-air.local>
2018-11-27 14:22:21 -0800

    Initial Commit

---
+++ Project1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Venkata Satya Harsha Vardhan Lolla on 11/27/18.
+//  Copyright © 2018 Venkata Satya Harsha Vardhan Lolla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
